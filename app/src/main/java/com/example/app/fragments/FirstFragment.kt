bed7c50aba04ef032567ffbf694030a9f28ef07b
Your Name <you@example.com>
2025-12-15 10:55:52 +0300

    добавлено установка приложения метро изменено:      app/src/main/AndroidManifest.xml 	изменено:      app/src/main/java/com/example/app/fragments/FirstFragment.kt 	новый файл:    app/src/main/res/drawable/ic_metro.png 	изменено:      app/src/main/res/layout/fragment_first.xml 	изменено:      app/src/main/res/layout/fragment_terminal.xml 	изменено:      app/src/main/res/values/strings.xml

--- app/src/main/java/com/example/app/fragments/FirstFragment.kt
+++ app/src/main/java/com/example/app/fragments/FirstFragment.kt
@@ -62,7 +62,8 @@ class FirstFragment : Fragment() {
             R.id.installmetamask to "https://github.com/definitly486/redmia5/releases/download/apk/metamask-blockchain-wall-android3-7.60.2-metamask-main-prod-3213.apk",
             R.id.installtermux to "https://github.com/definitly486/redmia5/releases/download/apk/termux-app_v0.118.3+github-debug_arm64-v8a.apk",
             R.id.installelektichka to "https://github.com/definitly486/Lenovo_Tab_3_7_TB3-730X/releases/download/apk/yandex_elektrichka+v.3.43.0-34300_arm64.apk",
-            R.id.installlibretube to "https://github.com/definitly486/redmia5/releases/download/apk/libretube-arm64-v8a-release.apk"
+            R.id.installlibretube to "https://github.com/definitly486/redmia5/releases/download/apk/libretube-arm64-v8a-release.apk",
+            R.id.installmetro to "https://github.com/definitly486/redmia5/releases/download/apk/ru.mosmetro.metro_8020_rs.apk"
         )
 
         // Назначаем обработчик события каждому элементу карты
