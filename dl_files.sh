8e6d232e52ab9f3d9dd31e8c50f0fc81b4aaa2aa
Your Name <you@example.com>
2026-02-09 12:22:55 +0300

    изменено:      dl_files.sh

--- dl_files.sh
+++ dl_files.sh
@@ -9,6 +9,6 @@ if ! [  -f "APatch-KSU.zip" ]; then
     curl -L -k  -o   APatch-KSU.zip  https://github.com/definitly486/redmia5/releases/download/root/APatch-KSU.zip
 fi
 
-if ! [  -f "APatch_11107_11107-release-signed.apk" ]; then
-    curl -L -k  -o   APatch_11107_11107-release-signed.apk  https://github.com/definitly486/redmia5/releases/download/apk/APatch_11107_11107-release-signed.apk
+if ! [  -f "KernelSU_v1.0.5_12081-release.apk" ]; then
+    curl -L -k  -o   APatch_11107_11107-release-signed.apk  https://github.com/definitly486/redmia5/releases/download/apk/KernelSU_v1.0.5_12081-release.apk
 fi
